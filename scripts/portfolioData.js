8e3a7928b7bca1534037441ce9d45d01de93544f
Peter Breen <pbreen@gmail.com>
2016-05-16 21:06:10 -0700

    add placeholder images

--- scripts/portfolioData.js
+++ scripts/portfolioData.js
@@ -5,7 +5,7 @@ var myProjects = [
     category:    'Code 201',
     projectUrl:   'http://peterbreen.github.io/welcome-to-seattle/',
     publishedOn: '2016-04-07',
-    featureImage: '',
+    featureImage: 'http://placehold.it/400x500',
     body:        '<p>Welcome To Seattle is my final project from Code Fellows Code 201 class. The concept behind this application was an app where the user could answer a few questions about themselves and their preferences and be given a ranked list of neighborhoods in Seattle based on those preferences to find a neighborhood for them to move to.</p>'
   },
   {
@@ -13,7 +13,7 @@ var myProjects = [
     category:    'Code 201',
     projectUrl:   'http://peterbreen.github.io/bus-mall/',
     publishedOn: '2016-04-01',
-    featureImage: '',
+    featureImage: 'http://placehold.it/400x500',
     body:        '<p>Bus Mall is my third major project from Code Fellows Code 201. Bus Mall is meant to emulate a SkyMall-esque service except for the lengthy mass transit commutes in the Puget Sound area. To maximize return on the future Bus Mall catalog, this site allows for user testing - users choose the best of 3 randomly selected products a certain number of times and at the end get to see a Chart.js bar chart showing total lifetime selections for all products - basically a visual representation of which products are most popular from all testers (via localStorage).</p>'
   },
   {
@@ -21,7 +21,7 @@ var myProjects = [
     category:    'Code 201',
     projectUrl:   'http://peterbreen.github.io/pizza-odyssey/index.html',
     publishedOn: '2016-03-26',
-    featureImage: '',
+    featureImage: 'http://placehold.it/400x500',
     body:        '<p>Do you want pizza? Do you want pizza from the FUTURE? Pizza 3001 is here for you. It consists of the main index page and a <a href="http://peterbreen.github.io/pizza-odyssey/sales-data.html">intranet sales data</a> page where the proprietor can track pizzas sold per branch and per timeslot so they know when to hire drivers and how many to hire at any given time. It also makes bad nerd jokes that Benton probably appreciated. Probably.</p>'
   },
   {
@@ -29,7 +29,7 @@ var myProjects = [
     category:    'Personal',
     projectUrl:   'http://peterbreen.info/cider/',
     publishedOn: '2015-09-08',
-    featureImage: '',
+    featureImage: 'http://placehold.it/400x500',
     body:        '<p>Cider Selector is a personal project my partner and I made to answer the question of what cider to buy if we were out at a bar - using a searchable data set of cider brand, variety, a score from 1-3, and some tasting notes. If we wonder if we have tried a particular cider before we can quickly search for it on this site and return relevant results. Responsive design first as the primary use is envisioned to be on a phone.</p>'
   },
 ];
