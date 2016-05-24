fda57811079ba2447ae451c81d72ea9262e882aa
Peter Breen <pbreen@gmail.com>
2016-05-23 17:02:31 -0700

    filled in function declarations for all 4 pages

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,1 +1,7 @@
 /* this is where page.js related things go */
+page.base('/');
+page('/', homeController.index);
+page('/projects', projectsController.index);
+page('/contact', contactController.index);
+page('/about', aboutController.index);
+page();
