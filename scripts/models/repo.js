5f1d986706a719853c420f76167339954552aafc
Peter Breen <pbreen@gmail.com>
2016-05-24 19:56:55 -0700

    adds unforked and linecount functions to filter/reduce repo ajax call into unforked repo number and combined size of all unforked repos

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -5,7 +5,7 @@
 
   repos.requestRepos = function(callback) {
     $.ajax({
-      url: 'https://api.github.com/users/' + gituser + '/repos' + '&sort=updated',
+      url: 'https://api.github.com/users/' + gituser + '/repos' + '?sort=updated',
       type: 'GET', headers: {'Authorization': 'token ' + gittoken},
       success: function(data, message, xhr) {
         repos.all = data;
@@ -19,5 +19,21 @@
       return repo[attr];
     });
   };
+
+  repos.unforked = function() {
+    return repos.all.filter(function(repo) {
+      return (repo.fork === false);
+    });
+  };
+
+  repos.lineCount = function(arr) {
+    return arr.map(function(repo) {
+      return repo.size;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
   module.repos = repos;
 })(window);
