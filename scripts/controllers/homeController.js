4a02be5a50219c0191b33d7204064ec03c4d8f2e
Peter Breen <pbreen@gmail.com>
2016-05-25 09:34:01 -0700

    fixes repeated git stat problem on page reload

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,6 +1,7 @@
 (function(module) {
   var homeController = {};
   homeController.index = function() {
+    $('#github-stats').empty();
     repos.requestRepos(repoView.index);
     $('section').hide();
     $('#home').show();
