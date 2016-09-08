511b0dca1c395067a0328a262acc84c0c9b7c55d
Peter Breen <pbreen@gmail.com>
2016-09-08 13:47:07 -0700

    comments out detail function call; needs proper placement

--- scripts/views/portfolioView.js
+++ scripts/views/portfolioView.js
@@ -39,7 +39,7 @@
         $('#category-filter').append(a.toHtml($('#category-filter-template')));
       };
       $('#projects').append(a.toHtml($('#project-render')));
-      $('#projects').append(a.toHtml($('#project-render-detail')));
+      // $('#projects').append(a.toHtml($('#project-render-detail')));
     });
     portfolioView.populateCatFilter();
     portfolioView.handleCatFilter();
