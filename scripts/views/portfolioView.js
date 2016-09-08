78f5720027746aecc212f9c9261a698b803f7506
Peter Breen <pbreen@gmail.com>
2016-09-08 14:58:48 -0700

    cleanup commented code

--- scripts/views/portfolioView.js
+++ scripts/views/portfolioView.js
@@ -22,16 +22,6 @@
       }
     });
   };
-  // uncomment this and the function call below to enable "read more" functionality
-
-  // portfolioView.setTeasers = function() {
-  //   $('.article-body *:nth-of-type(n+2)').hide();
-  //   $('#articles').on('click', 'a.read-on', function(e) {
-  //     e.preventDefault();
-  //     $(this).parent().find('*').fadeIn();
-  //     $(this).hide();
-  //   });
-  // };
 
   portfolioView.initIndexPage = function() {
     Project.all.forEach(function(a){
@@ -39,12 +29,14 @@
         $('#category-filter').append(a.toHtml($('#category-filter-template')));
       };
       $('#projects').append(a.toHtml($('#project-render')));
-      // $('#projects').append(a.toHtml($('#project-render-detail')));
     });
     portfolioView.populateCatFilter();
     portfolioView.handleCatFilter();
-    //uncomment this and setTeasers above to enable "read more" functionality
-    // portfolioView.setTeasers();
+    portfolioView.displayDetails = function() {
+      Project.all(function(a){
+        $('#projects').append(a.toHtml($('#project-render-detail')));
+      });
+    };
   };
   module.portfolioView = portfolioView;
 })(window);
