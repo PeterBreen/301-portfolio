f0578196f2b19319880e91a9683a605c1e14f8c2
Peter Breen <pbreen@gmail.com>
2016-09-08 12:50:00 -0700

    adds route for portfolio detail pages

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,5 +1,6 @@
 page('/', homeController.index);
 page('/projects', projectsController.index);
+page('/projects/:projectUrl', projectsController.detail);
 page('/contact', contactController.index);
 page('/blog', blogController.index);
 page();
