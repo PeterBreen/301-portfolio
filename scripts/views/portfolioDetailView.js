35fe174a22011f68a7e141001a02a4794cfa4bd4
Peter Breen <pbreen@gmail.com>
2017-12-22 12:18:35 -0800

    removed options argument from fn as test

--- scripts/views/portfolioDetailView.js
+++ scripts/views/portfolioDetailView.js
@@ -1,8 +1,8 @@
 //Helper function for variable-length lists, used in details page
-Handlebars.registerHelper('list', function(context, options) {
+Handlebars.registerHelper('list', function(context) {
   var ret = '<ul>';
   for(var i=0, j=context.length; i<j; i++) {
-    ret = ret + '<li>' + options.fn(context[i]) + '</li>';
+    ret = ret + '<li>' + context[i] + '</li>';
   }
   return ret + '</ul>';
 });
